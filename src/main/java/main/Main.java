fbc0492c14f5b914357cff14c4a96ee44c68ee64
hiyigh <limgh04@gmail.com>
2024-02-07 15:40:46 +0900

    tasks.test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,25 +3,21 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
+/*import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;*/
 
 @SpringBootApplication(
 		exclude = {
 				SecurityAutoConfiguration.class,
-				ContextInstanceDataAutoConfiguration.class,
+				/*ContextInstanceDataAutoConfiguration.class,
 				ContextStackAutoConfiguration.class,
-				ContextRegionProviderAutoConfiguration.class
+				ContextRegionProviderAutoConfiguration.class*/
 		}
 )
 public class Main {
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
