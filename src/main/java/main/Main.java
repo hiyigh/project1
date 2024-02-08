381556856bb20d7949f0de1e56c90e7d1a73c98e
hiyigh <limgh04@gmail.com>
2024-02-08 15:58:39 +0900

    no class def found error -> spring security error?

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,13 +3,11 @@ package main;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 @SpringBootApplication(
 		exclude = {
-				SecurityAutoConfiguration.class,
 				ContextInstanceDataAutoConfiguration.class,
 				ContextStackAutoConfiguration.class,
 				ContextRegionProviderAutoConfiguration.class
