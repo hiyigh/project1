95eaf3c6af3cb89174af99d504b41f20cb4e54eb
hiyigh <limgh04@gmail.com>
2024-02-10 03:25:32 +0900

    Databaseconfig value

--- src/main/java/main/configure/DatabaseConfig.java
+++ src/main/java/main/configure/DatabaseConfig.java
@@ -12,29 +12,15 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
-@Component
+@Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Value("${spring.datasource.connection-timeout}")
-    private int connectionTimeout;
-
-    @Value("${spring.datasource.maximum-pool-size}")
-    private int maximumPoolSize;
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().url("jdbc:mariadb://project1-blog.caw1uaxlfzuq.ap-northeast-2.rds.amazonaws.com:3306/project1-blog")
-                .username(username)
-                .password(password)
-                .driverClassName(driverClassName)
+                .username("admin123456")
+                .password("admin123456")
+                .driverClassName("org.mariadb.jdbc.Driver")
                 .build();
     }
     @Bean
